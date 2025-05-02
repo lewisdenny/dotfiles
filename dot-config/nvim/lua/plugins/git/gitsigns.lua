68769e38a338f8dc29431f11ee7f918a4a8d6a47
Lewis Denny <lewisdenny@me.com>
2025-05-02 10:40:02 +1000

    tidy gitsigns config

--- dot-config/nvim/lua/plugins/git/gitsigns.lua
+++ dot-config/nvim/lua/plugins/git/gitsigns.lua
@@ -13,38 +13,48 @@ return {
       on_attach = function(bufnr)
         local gitsigns = require "gitsigns"
 
+        function ToggleGitSignsBlame()
+          local all_windows = vim.api.nvim_list_wins() -- Get a list of all window IDs
+          local closedGitBlame = false
+
+          for _, win_id in ipairs(all_windows) do
+            if vim.api.nvim_win_is_valid(win_id) then -- Check if the window is still valid
+              local buf_nr = vim.api.nvim_win_get_buf(win_id) -- Get the buffer handle for the window
+              if vim.api.nvim_buf_is_valid(buf_nr) then
+                local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf_nr }) -- Get the filetype of the buffer
+                if filetype == "gitsigns-blame" then
+                  vim.api.nvim_win_close(win_id, false) -- Close the window, don't close the buffer
+                  closedGitBlame = true
+                end
+              end
+            end
+          end
+          if not closedGitBlame then
+            gitsigns.blame()
+          end
+        end
+
         local function map(mode, l, r, opts)
           opts = opts or {}
           opts.buffer = bufnr
           vim.keymap.set(mode, l, r, opts)
         end
 
-        -- Navigation
-        map("n", "]c", function()
-          if vim.wo.diff then
-            vim.cmd.normal { "]c", bang = true }
-          else
-            gitsigns.nav_hunk "next"
-          end
-        end, { desc = "Jump to next git [c]hange" })
+        -- These two functions allow the next and prev operations to be dot repeatable
+        _G._next_hunk_func = function()
+          gitsigns.nav_hunk "next"
+        end
 
-        map("n", "[c", function()
-          if vim.wo.diff then
-            vim.cmd.normal { "[c", bang = true }
-          else
-            gitsigns.nav_hunk "prev"
-          end
-        end, { desc = "Jump to previous git [c]hange" })
+        _G.previous_hunk_func = function()
+          gitsigns.nav_hunk "prev"
+        end
 
-        -- Actions
-        -- visual mode
-        map("v", "<leader>hs", function()
-          gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
-        end, { desc = "stage git hunk" })
-        map("v", "<leader>hr", function()
-          gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-        end, { desc = "reset git hunk" })
-        -- normal mode
+        -- stylua: ignore start
+
+        --g@ operator needs a textobject, l selects the character under the cursor but arn't using the selected textobject
+        map("n", "<leader>hn", function() vim.o.operatorfunc = "v:lua._next_hunk_func" vim.cmd.normal "g@l" end, {desc = "git [n]ext hunk"})
+        map("n", "<leader>hN", function() vim.o.operatorfunc = "v:lua.previous_hunk_func" vim.cmd.normal "g@l" end, {desc = "git previous hunk"})
+
         map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
         map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
         map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
@@ -53,13 +63,16 @@ return {
         map("n", "<leader>hb", gitsigns.blame_line, { desc = "git [b]lame line" })
         map("n", "<leader>hB", gitsigns.blame, { desc = "git [B]lame" })
         map("n", "<leader>hd", gitsigns.diffthis, { desc = "git [d]iff against index" })
-
-        map("n", "<leader>hD", function()
-          gitsigns.diffthis "@"
-        end, { desc = "git [D]iff against last commit" })
+        map("n", "<leader>hD", function() gitsigns.diffthis "@" end, { desc = "git [D]iff against last commit" })
+        map("v", "<leader>hs", function() gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "git [s]tage selected lines" })
+        map("v", "<leader>hr", function() gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "git [r]eset selected lines" })
 
         -- Toggles
-        map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
+        map("n", "<leader>ub", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
+
+        -- We don't use map() here so the keymap is global
+        vim.keymap.set("n", "<leader>uB", ToggleGitSignsBlame, { desc = "[T]oggle git [B]lame window" })
+        -- stylua: ignore end
       end,
     },
   },
