d7c6fd483804d86d5e8748776b738395802308e9
Lewis Denny <lewisdenny@me.com>
2025-05-06 14:45:08 +1000

    Add Hydra for debug mode

---
+++ dot-config/nvim/lua/plugins/hydra.lua
@@ -0,0 +1,38 @@
+return {
+  "nvimtools/hydra.nvim",
+  keys = { { "<leader>db" } },
+  config = function()
+    local Hydra = require "hydra"
+    local dap = require "dap"
+
+    Hydra {
+      name = "DEBUG",
+      mode = "n",
+      body = "<leader>db",
+      config = {
+        color = "pink",
+        hint = {
+          type = "window",
+        },
+        invoke_on_body = true,
+      },
+
+      -- stylua: ignore
+      heads = {
+        { "c", function() require("dap").continue() end, { desc = "Continue" } },
+        { "J", function () dap.step_over() end, { desc = "Step Over" } },
+        { "H", function () dap.step_out() end, { desc = "Step Out" }},
+        { "L", function () dap.step_into() end, { desc = "Step Into" }},
+        { "b", function () dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" } },
+        { "G", function () dap.run_to_cursor() end, { desc = "Run to Cursor" } },
+        { "r", function () dap.run_last() end, { desc = "Run last target" } },
+        { "R", function () dap.restart() end, { desc = "Restart" } },
+
+        { "<Esc>", nil, { exit = true } },
+        { "q", nil, { exit = true } },
+        { "jk", nil, { exit = true } },
+        { "x", function () dap.terminate() end, { exit = true } },
+      },
+    }
+  end,
+}
--- dot-config/nvim/lua/plugins/lualine.lua
+++ dot-config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,8 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local lazy_status = require "lazy.status"
+      local hydra_statusline = require "hydra.statusline"
+
       require("lualine").setup {
         options = {
           disabled_filetypes = {
@@ -24,7 +26,24 @@ return {
           "mason",
         },
         sections = {
-          lualine_a = { "mode" },
+          lualine_a = {
+            {
+              "mode",
+              fmt = function(res)
+                local hydra_active = hydra_statusline.is_active()
+                return hydra_active and " " .. hydra_statusline.get_name() or " " .. res
+              end,
+              color = function(_)
+                if hydra_statusline.is_active() then
+                  if hydra_statusline.get_name() == "DEBUG" then
+                    return { bg = "#e06c75", gui = "bold" }
+                  end
+                else
+                  return { gui = "bold" }
+                end
+              end,
+            },
+          },
           lualine_b = { "branch", "diff", "diagnostics" },
           lualine_c = { "filename" },
           lualine_x = {
