269b3aaa09af18a4939ddf356a93fe37a073b32e
Lewis Denny <lewisdenny@me.com>
2025-05-06 14:57:10 +1000

    update duck config, reduce movement

--- dot-config/nvim/lua/plugins/duck.lua
+++ dot-config/nvim/lua/plugins/duck.lua
@@ -1,16 +1,16 @@
 return {
   "tamton-aquib/duck.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<leader>dh", function() require("duck").hatch() end, desc = "Duck Hatch"},
-    },
+  cmd = { "Duckhatch", "Duckcook" },
   config = function()
-    vim.keymap.set("n", "<leader>dc", function()
-      require("duck").cook()
-    end, { desc = "Duck Cook" })
-
-    vim.keymap.set("n", "<leader>dC", function()
+    vim.api.nvim_create_user_command("Duckhatch", function()
+      require("duck").hatch("🦆", 2)
+    end, {
+      desc = "Hatch a duck",
+    })
+    vim.api.nvim_create_user_command("Duckcook", function()
       require("duck").cook_all()
-    end, { desc = "Duck Cook All" })
+    end, {
+      desc = "Cook all the ducks",
+    })
   end,
 }
