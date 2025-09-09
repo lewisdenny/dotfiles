54261c987fdad14df48f1aa2472db3ad0615662e
Lewis Denny <lewisdenny@me.com>
2025-09-09 15:18:52 +1000

    Remove oh-my-posh

--- dot-config/zsh/plugins.zsh
+++ dot-config/zsh/plugins.zsh
@@ -3,10 +3,5 @@ export BAT_THEME=tokyonight_night
 # Better zsh history
 eval "$(atuin init zsh --disable-up-arrow)"
 
-# oh-my-posh prompt
-# if [[ -z "${RUNNING_IN_LIMA}" ]];then
-#   eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.yaml)"
-# fi
-
 # Starship
 eval "$(starship init zsh)"
