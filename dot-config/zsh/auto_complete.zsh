7b7e3b2938d1680d8c106bfcf421c192022352fe
Lewis Denny <lewisdenny@me.com>
2025-09-09 15:19:14 +1000

    Speed up zsh compinit by using smart caching

--- dot-config/zsh/auto_complete.zsh
+++ dot-config/zsh/auto_complete.zsh
@@ -1,6 +1,25 @@
 source <(fzf --zsh)
 
-autoload -U +X compinit
+# {{
+# Loading compinit is by far the slowest thing on startup
+# Here we setup caching for compinit with the .zcompdump file
+
+# Set custom location for the completion dump file
+mkdir -p "$HOME/.local/state/zsh/"
+ZCOMPDUMP_FILE="$HOME/.local/state/zsh/.zcompdump"
+
+# Check if the dump file exists and is newer than the completion functions
+if [[ -f "$ZCOMPDUMP_FILE" && "$ZCOMPDUMP_FILE" -nt "$fpath[1]" ]]; then
+    # Load the dump file if it's up-to-date
+    autoload -Uz compinit && compinit -d "$ZCOMPDUMP_FILE"
+    echo "loaded from cache"
+else
+    # Otherwise, generate a new dump file
+    echo "Creating cache"
+    autoload -Uz compinit && compinit -C -d "$ZCOMPDUMP_FILE"
+fi
+# }}
+
 zstyle ':completion:*' menu select
 zmodload zsh/complist
 compinit
