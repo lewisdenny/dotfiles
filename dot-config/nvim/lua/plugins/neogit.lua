1528202a0998dc76d8998d1e860684dbf348eb83
Lewis Denny <lewisdenny@me.com>
2025-09-22 20:03:45 +1000

    remove unneeded custom servers

--- dot-config/nvim/lua/plugins/neogit.lua
+++ dot-config/nvim/lua/plugins/neogit.lua
@@ -17,13 +17,6 @@ neogit.setup {
   log_view = {
     kind = "replace",
   },
-  -- Add custom gitlab server
-  git_services = {
-    ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-    ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-    ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-    ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
-  },
 }
 
 vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neogit", silent = true })
